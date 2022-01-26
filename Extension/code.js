8e060248c41a559bfaa6ed061ceb1e4c7b6e62a4
hambagah <90470474+hambagah@users.noreply.github.com>
2022-01-26 13:59:28 -0500

    Add files via upload

---
+++ Extension/code.js
@@ -0,0 +1,8 @@
+console.log("bruh");
+
+const imgs = document.getElementsByTagName("img");
+
+let paragraphs = getElementsbyTagName('p');
+for (elt of paragraphs){
+    elt.style['background-color'] = '#FF0000';
+}
